07519fb390dadf4464c9664391f4dfb8a0b53787
Geremia <Geremia@users.noreply.github.com>
2016-04-15 11:24:06 -0700

    It's technically LCC (Library of Congress Classification), not LCN.

--- ISBN_to_LCN.bash
+++ ISBN_to_LCN.bash
@@ -1,14 +1,14 @@
 #!/usr/bin/bash
 
-#Search LOC.gov and OCLC Classify for Library of Congress Numbers (LCNs) associated with ISBNs of text file (arg. #1), outputting into one output file (arg. #2).
-# If LCN is not found, Dewey tried; if neither found, output will not contain a record for the particular ISBN.
+#Search LOC.gov and OCLC Classify for Library of Congress Classification (LCC) numbers associated with ISBNs of text file (arg. #1), outputting into one output file (arg. #2).
+# If LCC is not found, Dewey tried; if neither found, output will not contain a record for the particular ISBN.
 
 input=`cat ${1:?First argument missing: File containing list of ISBNs required.} | sort -nu | sed '/^$/d'`
-output=${2:?Second argument missing: Name of file containing found LCNs required.}
+output=${2:?Second argument missing: Name of file containing found LCCs required.}
 
 rm -f .tmp?.txt
 
-echo -e "\nFinding LCNs in LOC.gov...\n"
+echo -e "\nFinding LCCs in LOC.gov...\n"
 for i in ${input}
 do
     xml=`curl -s "http://lx2.loc.gov:210/lcdb?operation=searchRetrieve&query=bath.isbn=$i&maximumRecords=1&recordSchema=mods"` #https://stackoverflow.com/a/27877972/1429450
@@ -16,12 +16,12 @@ do
     title=`echo $xml | grep -o "<title>[^<]*</title>" | sed 's/<title>\([^>]*\)<\/title>/\1/' | tr '\n' ' ' | cut -c-32`
     author=`echo $xml | grep -o "<namePart>[^<]*</namePart>" | sed 's/<namePart>\([^>]*\)<\/namePart>/\1/' | head -n 1 | tr '\n' ' ' | cut -c-32`
 
-    if [[ $lcc ]]; then #if LCN found
+    if [[ $lcc ]]; then #if LCC found
 	printf "%-24s | %13s | %-32s | %-32s\n" "$lcc" "$i" "$title" "$author" | tee -a .tmp1.txt
     fi    
 done
 
-echo -e "\nFinding LCNs in OCLC Classify...\n"
+echo -e "\nFinding LCCs in OCLC Classify...\n"
 for i in ${input}
 do
     xml=`curl -s "http://classify.oclc.org/classify2/Classify?isbn=$i&summary=true"`
@@ -33,12 +33,12 @@ do
 	lcc=`echo $xml | grep -oP 'nsfa="\K[0-9][^"]*(?=")' | tail -1`
     fi
 
-    if [[ $lcc ]]; then #if LCN found
+    if [[ $lcc ]]; then #if LCC found
 	printf "%-24s | %13s | %-32s | %-32s\n" "$lcc" "$i" "$title" "$author" | tee -a .tmp2.txt
     fi    
 done
 
-echo -e "\nMerging duplicate ISBNs (preferring LCNs from LOC.gov)..."
+echo -e "\nMerging duplicate ISBNs (preferring LCCs from LOC.gov)..."
 sort -bunt '|' -k2,2 .tmp1.txt .tmp2.txt | sort -bt '.' -k1,1 | tee "${output}" #select 1st occurrence (LOC result) of duplicate ISBNs only
 echo -e "\nOutput written to ${output}."
 
